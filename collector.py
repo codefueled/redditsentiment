510c7680083e528ff8427ef27e44a6a3336fc31e
Alexander Dougherty <alexander.c.dougherty@vanderbilt.edu>
2019-06-14 22:07:06 -0400

    Added SQL connection and CREATE TABLE

--- collector.py
+++ collector.py
@@ -1,9 +1,22 @@
 import praw
+import mysql.connector
+
+#SQL INIT
+redditDB = mysql.connector.connect(
+    user = 'root',
+    host = 'localhost',
+    database = 'redditproject'
+)
+
+mycursor = redditDB.cursor()
+mycursor.execute("CREATE TABLE Hiphopheads (comment_ID INT PRIMARY KEY AUTO_INCREMENT, albumname VARCHAR(100), body VARCHAR(10000), sentiment INT);")
 
 reddit = praw.Reddit(client_id='j7Afg69TlZOI2A',
                      client_secret='Gk1BAnzZbPtK6YgZqRaQ8814v1g', password='Romoregis1!',
                      user_agent='untitled', username='redditalbumvisual')
 
 for submission in reddit.subreddit('hiphopheads').top('all', limit = 1500):
-    if not submission.stickied and '[FRESH ALBUM]' in submission.title:
-        print(submission.title)
+    if not submission.stickied and '[FRESH ALBUM] Denzel Curry - ZUU' in submission.title:
+        #Considers only top-level comments (not replies)
+        for comment in submission.comments:
+            print(comment.body)
