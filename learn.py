8da143bbba453038c4ae84c89082eebce0fde21a
Alex <alexchea@seas.upenn.edu>
2019-06-29 19:29:50 -0400

    sentiment insertion

---
+++ learn.py
@@ -0,0 +1,11 @@
+import textfiltersandprocessors as f_methods 
+import os.path
+from os import path 
+
+
+random_comments = f_methods.generate_random_comment_to_token_list()
+
+if not(path.exists("testing.txt")):
+    f_methods.comments_to_file("testing.txt", random_comments)
+    
+f_methods.upload_sentiment_DB("back_up_testing.txt")
--- textfiltersandprocessors.py
+++ textfiltersandprocessors.py
@@ -1,9 +1,36 @@
 #Non-member functions to filter and process text before and after SQL data creation
-import mysql.connector
+import reddit_db as DB 
+import re 
 
 def removeUnicodeFromComment(commentbody):
     commentbody = commentbody.encode('ascii', errors='ignore')
     return commentbody
 
+def generate_random_comment_to_token_list():
+    DB.mycursor.execute("SELECT comment_ID, body FROM hiphopheads ORDER BY RAND (0.5) LIMIT 1000")
+    comments = DB.mycursor.fetchall()
+    return comments 
 
+def comments_to_file(filename, comments): 
+    with open(filename, 'w') as file_object:
+        for comment in comments:
+            file_object.write("***" + str(comment[0]) + "***\n")
+            file_object.write(comment[1] + "\n")
+            file_object.write("######\n")
+                              
+def upload_sentiment_DB(filename):
+    ID = 0
+    sentiment = 0
+    with open(filename) as file_object:
+        for line in file_object:
+            if(re.search("^\*\*\*.*\*\*\*$", line)):
+                ID = int(line.replace("*", ""))
+            elif(re.search("^###.*###$", line)):
+                sentiment = int(line.replace("#", ""))
+                sql = "UPDATE hiphopheads SET sentiment = %s WHERE comment_ID = %s"
+                val = (sentiment,ID)
+                DB.mycursor.execute(sql, val)
+            else: 
+                continue 
+    
     
