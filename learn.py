64ac2af8a1f79b115c1212a17f5f0783d4a7b9a9
Alexander Dougherty <alexander.c.dougherty@vanderbilt.edu>
2019-06-30 13:34:25 -0400

    Created token file

--- learn.py
+++ learn.py
@@ -3,9 +3,21 @@ import os.path
 from os import path 
 
 
-random_comments = f_methods.generate_random_comment_to_token_list()
+#random_comments = f_methods.fetch_random_comment_to_token_list()
 
 if not(path.exists("testing.txt")):
     f_methods.comments_to_file("testing.txt", random_comments)
     
-f_methods.upload_sentiment_DB("testing.txt")
+#f_methods.upload_sentiment_DB("testing.txt")
+
+
+labeled_comments = f_methods.fetch_comments_with_sentiment()
+tokenlist = list()
+for comment in labeled_comments:
+    tokenlist.append(f_methods.comment_to_tokens(comment[0]))
+
+with open('tokens.txt', 'w') as file_object:
+    for tokens in tokenlist:
+        for token in tokens:
+            file_object.write(token + " ")
+        file_object.write('\n\n')
--- textfiltersandprocessors.py
+++ textfiltersandprocessors.py
@@ -1,15 +1,22 @@
 #Non-member functions to filter and process text before and after SQL data creation
 import reddit_db as DB 
-import re 
+import re
+import string
+from nltk.stem import PorterStemmer
 
 def removeUnicodeFromComment(commentbody):
     commentbody = commentbody.encode('ascii', errors='ignore')
     return commentbody
 
-def generate_random_comment_to_token_list():
+def fetch_random_comment_to_token_list():
     DB.mycursor.execute("SELECT comment_ID, body FROM hiphopheads ORDER BY RAND (0.5) LIMIT 1000")
     comments = DB.mycursor.fetchall()
-    return comments 
+    return comments
+
+def fetch_comments_with_sentiment():
+    DB.mycursor.execute("SELECT body FROM hiphopheads WHERE sentiment IS NOT NULL")
+    comments = DB.mycursor.fetchall()
+    return comments
 
 def comments_to_file(filename, comments): 
     with open(filename, 'w') as file_object:
@@ -32,5 +39,21 @@ def upload_sentiment_DB(filename):
                 DB.mycursor.execute(sql, val)
             else: 
                 continue 
-    
-    
+
+def comment_to_tokens(comment):
+    #Split comment into word tokens
+    tokens = comment.split()
+    #Create trans table to map punctuation to empty string
+    map = str.maketrans('', '', string.punctuation)
+    #Replace punctuation
+    tokens = [t.translate(map) for t in tokens]
+    #Remove non-alphabetic
+    tokens = [token for token in tokens if token.isalpha()]
+    #Filter out one-letter tokens
+    tokens = [token for token in tokens if len(token) > 1]
+    #Converts to lowercase
+    tokens = [t.lower() for t in tokens]
+    #Stemming
+    ps = PorterStemmer()
+    tokens = [ps.stem(t) for t in tokens]
+    return tokens
